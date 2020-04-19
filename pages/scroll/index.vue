3e74662e858101ecd5588c097354bd8a200ac358
kmrtmk03 <krmmmmmmrk@gmail.com>
2020-04-19 11:15:05 +0900

    Add ScrollPage Content

--- pages/scroll/index.vue
+++ pages/scroll/index.vue
@@ -1,10 +1,20 @@
 <template>
-  <section class="container">
+  <div class="container">
     <Artwork />
     <div class="content">
-      <h1 class="title">Scroll</h1>
+      <section class="section--0">
+        <h1 class="title">Scroll</h1>
+      </section>
+      <section class="section section--1">
+        <h2 class="section--title">Section-1</h2>
+        <p>ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+      </section>
+      <section class="section section--2">
+        <h2 class="section--title">Section-2</h2>
+        <p>ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト</p>
+      </section>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
@@ -22,7 +32,7 @@ export default {
 .content {
   position: relative;
   z-index: 2;
-  height: 500vh;
+  min-height: 500vh;
   background-color: rgba(255, 255, 255, 0.9);
 }
 .title {
@@ -31,5 +41,30 @@ export default {
   text-align: center;
   font-family: 'Helvetica';
   font-size: 10vw;
+}
+.section {
+  width: 90vw;
+  background-color: #fff;
+  margin: 0 auto 1rem;
+  padding: 0 1rem 1rem;
+  @include mq(md) {
+    margin: 0 auto 3rem;
+    padding: 0 2rem 2rem;
+  }
+  &--title {
+    font-size: 2.0rem;
+    text-align: center;
+    padding: 1.0rem 0;
+    @include mq(md) {
+      font-size: 2.4rem;
+      padding: 2.0rem 0;
+    }
+  }
+  &>p {
+    font-size: 1.0rem;
+    @include mq(md) {
+      font-size: 1.4rem;
+    }
+  }
 }
 </style>
